4dbc27a2fc72fdb069cb097a05a9c7e698af03ad
03 Torsin <26248@bbw.ac.th>
2026-02-23 12:50:01 +0700

    Gemini
    ช่วยพัฒนาปรับปรุงโปรแกรมนี้ให้สมบูรณ์แบบยิ่งขึ้นและสะดวกต่อการใช้งาน พร้อมใส่ฟังก์ชั่นสุ่มคำชม แต่ล่ะเกรด และคำชื่นชมสำหรับเกรด F เพื่อให้พยายามต่อ พร้อมทั้งใส่สีตกแต่งเกรดแต่ละระดับ รวมถึง hello message และ ชื่อผู้พัฒนา และ Add Clear Screen , Visual Feed back , Robust Code เพิ่มฟังก์ชั่นบันทึกเกรด และใส่ชื่อ เพิ่มคำสั่งการเช็คประวัติ

---
+++ final03 (1).py
@@ -0,0 +1,13 @@
+print("สวัสดีครับ ยินดีต้อนรับสู่โปรแกรมคำนวณเกรดจากคะแนน")
+print("ชื่อผู้พัฒนา : นายญาณพัฒน์ ปุยละเทิม ม. 4/4 เลขที่ 3")
+score = int(input("Enter your score : "))
+if score >= 80:
+            print("Grade A ")
+elif score >= 70:
+            print("Grade B ")
+elif score >= 60:
+            print("Grade C ")
+elif score >= 50:
+            print("Grade D ")
+else:
+            print("Grade F ")
